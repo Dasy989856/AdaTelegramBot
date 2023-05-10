5ea42b725a4b40d569bdea7bbc587651ba73afd6
Dmitriy Gurkin <dasy989856@gmail.com>
2023-05-10 04:03:07 +0300

    edit login

--- internal/telegram/handlerCbq.go
+++ internal/telegram/handlerCbq.go
@@ -8,35 +8,35 @@ import (
 )
 
 // Обработчик CallbackQuery.
+// func (b *BotTelegram) handlerCbq(cbq *tgbotapi.CallbackQuery) error {
+// 	fmt.Println(cbq.Message.Text)
+// 	fmt.Println(cbq.Data)
+
+// 	// Определение типа cbq.
+// 	cbqPart := strings.Split(cbq.Data, ":")
+// 	if len(cbqPart) < 1 || len(cbqPart) > 2 {
+// 		return fmt.Errorf("error len cbqPart in static, cbqData: %s", cbq.Data)
+// 	}
+
+// 	// Статический cbq.
+// 	if len(cbqPart) == 1 {
+// 		if err := handlerCbqStatic(b, cbq); err != nil {
+// 			return err
+// 		}
+// 	}
+
+// 	// Динамический cbq.
+// 	// if len(cbqPart) == 2 {
+// 	//  TODO
+// 	// }
+
+// 	return nil
+// }
+
 func (b *BotTelegram) handlerCbq(cbq *tgbotapi.CallbackQuery) error {
-	fmt.Println(cbq.Message.Text)
-	fmt.Println(cbq.Data)
-
-	// Определение типа cbq.
-	cbqPart := strings.Split(cbq.Data, ":")
-	if len(cbqPart) < 1 || len(cbqPart) > 2 {
-		return fmt.Errorf("error len cbqPart in static, cbqData: %s", cbq.Data)
-	}
-
-	// Статический cbq.
-	if len(cbqPart) == 1 {
-		if err := handlerCbqStatic(b, cbq); err != nil {
-			return err
-		}
-	}
-
-	// Динамический cbq.
-	// if len(cbqPart) == 2 {
-	//  TODO
-	// }
-
-	return nil
-}
-
-func handlerCbqStatic(b *BotTelegram, cbq *tgbotapi.CallbackQuery) error {
 	cbqSteps := strings.Split(cbq.Data, ".")
 	if len(cbqSteps) < 1 {
-		return fmt.Errorf("error len cbqSteps in static, cbqData: %s", cbq.Data)
+		return fmt.Errorf("error len cbqSteps. cbqData: %s", cbq.Data)
 	}
 
 	switch cbqSteps[0] {
@@ -45,7 +45,7 @@ func handlerCbqStatic(b *BotTelegram, cbq *tgbotapi.CallbackQuery) error {
 			return err
 		}
 	case "ad_event":
-		if err := cbqHandlerAdEvent(b, cbq); err != nil {
+		if err := handlerCbqAdEvent(b, cbq); err != nil {
 			return err
 		}
 	}
